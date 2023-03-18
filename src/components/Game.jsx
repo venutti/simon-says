49070922c469ada08898abb8c3992e81da4684e3
venutti <lvenutti@fi.uba.ar>
2023-03-18 20:18:56 -0300

    Agrega funciones de game
    Agrega la funcionalidad básica para que el juego se ejecute correctamente

---
+++ src/components/Game.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+
+const Game = () => {
+  const colors = ["green", "yellow", "red", "blue"];
+  const [sequence, setSequence] = useState([]);
+  const [userSequence, setUserSequence] = useState([]);
+  const [round, setRound] = useState(1);
+
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
+
+  // Funciones del juego
+  const init = () => {
+    setSequence([getRandomColor()]);
+  };
+  const chooseColor = (color) => {
+    setUserSequence((prevUserSequence) => [...prevUserSequence, color]);
+  };
+  const addColor = () => {
+    setSequence((prevSequence) => [...prevSequence, getRandomColor()]);
+    setRound((prevRound) => prevRound + 1);
+    setUserSequence([]);
+  };
+  const reset = () => {
+    setSequence([]);
+    setUserSequence([]);
+    setRound(0);
+  };
+
+  useEffect(() => {
+    if (sequence.length === 0) {
+      init();
+      return;
+    }
+
+    // No hay movimientos
+    if (userSequence.length === 0) return;
+
+    for (let i = 0; i < userSequence.length; i++) {
+      if (sequence[i] !== userSequence[i]) {
+        // Mala secuencia
+        reset();
+        addColor();
+        return;
+      }
+    }
+
+    if (sequence.length !== userSequence.length) return;
+
+    // Son iguales
+    addColor();
+  }, [userSequence]);
+
+  console.log("game", sequence);
+  console.log("user", userSequence);
+  console.log("round", round);
+
+  const renderedPanels = colors.map((color) => (
+    <div key={color} className="game__panel" onClick={() => chooseColor(color)}>
+      {color}
+    </div>
+  ));
+
+  return <div className="game">{renderedPanels}</div>;
+};
+
+export default Game;
